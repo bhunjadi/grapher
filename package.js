d17332ad119157cfd1f640b4a12e657baf8b4839
harryadel <harryadelb@gmail.com>
2025-08-20 16:29:41 +0300

    Replace dburles:mongo-collection-instances with built-in Mongo.getCollection

--- package.js
+++ package.js
@@ -31,13 +31,10 @@ Package.onUse(function (api) {
     'mongo',
 
     // https://github.com/Meteor-Community-Packages/meteor-collection-hooks/
-    'matb33:collection-hooks@2.0.0',
+    'matb33:collection-hooks@2.1.0-beta.4',
 
     // https://github.com/Meteor-Community-Packages/meteor-publish-composite
     'reywood:publish-composite@1.9.0',
-
-    // https://github.com/Meteor-Community-Packages/mongo-collection-instances
-    'dburles:mongo-collection-instances@1.0.0',
 
     // https://github.com/harryadel/meteor-subscription-scope
     'harry97:subscription-scope@0.6.0',
@@ -67,7 +64,6 @@ Package.onTest(function (api) {
     'underscore',
     'matb33:collection-hooks@2.0.0',
     'reywood:publish-composite@1.9.0',
-    'dburles:mongo-collection-instances@1.0.0',
     'harry97:subscription-scope@0.5.0',
     'herteby:denormalize@0.7.0',
     'mongo',
